2c5b98e93c25f606c2a4ba343a4c3f1d0bc91ee8
Vu Quoc Anh <quocanh20705@gmail.com>
2025-11-03 17:20:21 +0700

    fix: Add full content to Student.java

--- src/main/java/com/student/project/Student.java
+++ src/main/java/com/student/project/Student.java
@@ -1,14 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.student.project;
+
+import java.util.Date;
 
 /**
  *
  * @author quoca
  */
+
 public class Student {
+
+    private int id;
+    private String name;
+    private String email;
+    private Date dateOfBirth;
+
+    public Student(int id, String name, String email, Date dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + ", email=" + email + ", dateOfBirth=" + dateOfBirth + '}';
+    }
 
 }
